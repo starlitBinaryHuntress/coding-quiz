3f247791b3d371dca3706df2812c701ada369d56
Jordan <jnbarringer96@gmail.com>
2022-10-04 04:32:55 -0500

    Added start button, header, questions, answers, in HTML and began javascript

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+var start = document.querySelector("#start");
+
+start.addEventListener("click", startQuiz);
+
+function startQuiz () {
+    //create a timer
+    //let the first question and possible answers be shown
+}
